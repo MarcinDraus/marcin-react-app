077b5a6a035ccd051e4bff51ebfe0c8e34abd8df
MarcinDraus <marcin.drauss@gmail.com>
2023-06-01 13:41:57 +0200

    react first app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Hero from './components/Hero/Hero.js';
+import SearchForm from './components/SearchForm/SearchForm.js';
+import List from './components/List/List.js';
+
+const App = () => {
+  return (
+    <div>
+      <Hero />
+      <SearchForm />
+      <List />
+    </div>
+  );
+};
+
+export default App;
+
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line
+import styles from './SearchForm.module.scss';
+
+const SearchForm = () => {
+    return (
+        <form className={styles.searchForm}>
+            <input type="text" className={styles.input}/>
+            <button type="search" className={styles.button} >Search</button>
+        </form>
+    );
+}
+
+  export default SearchForm;
