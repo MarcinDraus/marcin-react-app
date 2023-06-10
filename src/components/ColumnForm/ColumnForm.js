223cfc19efef8b5e025da2e163543230695d4162
MarcinDraus <marcin.drauss@gmail.com>
2023-06-10 09:15:43 +0200

    ColumnForm - add new class and new component and Rendered cards

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,5 +1,7 @@
 import styles from './ColumnForm.module.scss';
 import { useState } from 'react';
+import Button from '../Button/Button';
+import TextInput from '../TextInput/TextInput';
 
 const ColumnForm = props => {
     const [title, setTitle] = useState('');
@@ -7,16 +9,19 @@ const ColumnForm = props => {
     const handleSubmit = e => {
         e.preventDefault();
         props.action({ title: title, icon: icon });
+          
         setTitle('');
         setIcon('');
     };
 
 	return (
-        <form onSubmit={handleSubmit}>
-            Title: <input type="text" value={title} onChange={e => setTitle(e.target.value)} />
-            Icon: <input type="text" value={icon} onChange={e => setIcon(e.target.value)} />
-            <button>Add column</button>
-        </form>
+    <form onSubmit={handleSubmit} className={styles.columnForm}>
+        <span className={styles.fieldName}>Title:</span>
+        <TextInput value={title} onChange={e => setTitle(e.target.value)} className={styles.input} />
+        <span className={styles.fieldName}>Icon:</span>
+        <TextInput value={icon} onChange={e => setIcon(e.target.value)} className={styles.input} />
+        <Button>Add column</Button>
+    </form>
 	);
 };
 
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,18 +3,44 @@ import Column from './../Column/Column';
 import ColumnForm from './../ColumnForm/ColumnForm';
 import { useState } from 'react';
 import shortid from 'shortid';
+//import Card from './../Card/Card';
 
 const List = () => {
     const [columns, setColumns] = useState([
-        { id: 1, title: 'Books', icon: 'book' },
-        { id: 2, title: 'Movies', icon: 'film' },
-        { id: 3, title: 'Games', icon: 'gamepad' }
+      {
+        id: 1,
+        title: 'Books',
+        icon: 'book',
+        cards: [
+          { id: 1, title: 'This is Going to Hurt' },
+          { id: 2, title: 'Interpreter of Maladies' }
+        ]
+      },
+      {
+        id: 2,
+        title: 'Movies',
+        icon: 'film',
+        cards: [
+          { id: 1, title: 'Harry Potter' },
+          { id: 2, title: 'Star Wars' }
+        ]
+      },
+      {
+        id: 3,
+        title: 'Games',
+        icon: 'gamepad',
+        cards: [
+          { id: 1, title: 'The Witcher' },
+          { id: 2, title: 'Skyrim' }
+        ]
+      }
     ]);
-
+      // eslint-disable-next-line no-lone-blocks
+      {/* zwraca tablicę, gdzie pierwszy element to referencja do wartości stanu, a druga to referencja do funkcji do jego modyfikacji. Dzięki użyciu destrukturyzacji, od razu przypisujemy te referencje do stałych – columns i setColumns*/}
     const addColumn = newColumn => {
-      setColumns([...columns, { id: shortid(), title: newColumn.title, icon: newColumn.icon }]);
-  };
-
+        setColumns([...columns, { id: shortid(), title: newColumn.title, icon: newColumn.icon, cards: [] }]);
+    };
+    
 	return (
 		<div className={styles.list}>
 			<header className={styles.header}>
@@ -22,7 +48,9 @@ const List = () => {
 			</header>
 			<p className={styles.description}>Interesting things I want to check out</p>
 			<section className={styles.columns}>
-				{columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
+        {columns.map(column => <Column key={column.id} id={column.id} title={column.title} icon={column.icon} cards={column.cards} />)}
+        {/*W pliku Column.js znajduje się definicja komponentu Column, który renderuje strukturę HTML dla pojedynczej kolumny. Ten komponent jest wykorzystywany w pliku List.js do renderowania wielu kolumn na podstawie danych z tablicy columns. W tym pliku, komponent Column jest zdefiniowany i renderuje strukturę HTML dla pojedynczej kolumny. Właściwości title i icon są używane do wyświetlania odpowiednich danych wewnątrz komponentu.Atrybut key ma być w założeniu po prostu unikalną wartością, po której React będzie w stanie łatwiej identyfikować elementy w tablicy.*/}
+       
 			</section>
 			<ColumnForm action={addColumn} />
 		</div>
