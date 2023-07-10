c66eb136f21c7f943695664815ae6dd235f0a9e7
MarcinDraus <marcin.drauss@gmail.com>
2023-07-10 16:56:54 +0200

    Routes> Route path

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import NavBar from './components/NavBar/NavBar.js';
 import Home from './components/Home/Home.js';
 import About from './components/About/About.js';
 import Favorite from './components/Favorite/Favorite.js';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router';
+import NotFound from './components/NotFound/NotFound.js';
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/favorite" element={<Favorite />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Container>
    </main>
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import PageTitle from "../PageTitle/PageTitle";
+//import Styles from './NotFound.module.scss';
+
+const NotFound = () => {
+    return(
+        <><PageTitle>404 Not Found</PageTitle></>
+    );
+
+};
+
+export default NotFound;
