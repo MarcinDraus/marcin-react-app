7c091f980c2182db537e92d07976d8a5ab311639
MarcinDraus <marcin.drauss@gmail.com>
2023-07-10 15:44:31 +0200

    Routes, Route

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import NavBar from './components/NavBar/NavBar.js';
 import Home from './components/Home/Home.js';
 import About from './components/About/About.js';
 import Favorite from './components/Favorite/Favorite.js';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <main>
-     <NavBar />
-     <Container>
-      <Home />
-      <About />
-      <Favorite />
-     </Container>
-    </main>
+      <NavBar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/favorite" element={<Favorite />} />
+        </Routes>
+      </Container>
+   </main>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -6,25 +6,17 @@ import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
+import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.render(
-
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.querySelector('#root')
 );
-// import ReactDOM from 'react-dom';
-// import App from './App';
-// import './styles/normalize.scss';
-// import './styles/global.scss';
-// import 'font-awesome/css/font-awesome.min.css';
-
-// ReactDOM.render(
-//   <App />,
-//   document.querySelector('#root')
-// );
 
